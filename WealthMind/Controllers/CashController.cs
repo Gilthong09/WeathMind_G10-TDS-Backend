61e2fb75d26b5b20f77059fc1b4cbd3b6154bea4
SAMIL QUEZADA <20222170@itla.edu.do>
2025-03-08 12:02:57 -0400

    feat: add saving and transaction controllers with CRUD operations

---
+++ WealthMind/Controllers/CashController.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Net.Mime;
+using WealthMind.Core.Application.DTOs.Account;
+using WealthMind.Core.Application.Enums;
+using WealthMind.Core.Application.Interfaces.Services;
+using WealthMind.Core.Application.ViewModels.CashV;
+using WealthMind.Infrastructure.Identity.Services;
+
+namespace WealthMind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Cash")]
+    public class CashController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ICashService _cashService;
+
+        public CashController(IMapper mapper, ICashService cashService)
+        {
+            _mapper = mapper;
+            _cashService = cashService;
+        }
+
+        [HttpPost("add")]
+        [SwaggerOperation(
+            Summary = "Registers a cash transaction.",
+            Description = "Recieves the necessary parameters for registering a cash transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(SaveCashViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Add(SaveCashViewModel dto)
+        {
+            // var request = _mapper.Map<SaveCashViewModel>(dto);
+
+            var results = await _cashService.Add(dto);
+            if (results.HasError)
+            {
+                return BadRequest(results);
+            }
+            return Ok(results);
+
+        }
+
+        [HttpGet("get")]
+        [SwaggerOperation(
+            Summary = "Gets a cash transaction.",
+            Description = "Recieves the necessary parameters for getting a cash transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveCashViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(string id)
+        {
+            var results = await _cashService.GetByIdSaveViewModel(id);
+            if (results.HasError)
+            {
+                return BadRequest(results);
+            }
+            return Ok(results);
+        }
+
+        [HttpGet("getall")]
+        [SwaggerOperation(
+            Summary = "Gets all cash transactions.",
+            Description = "Recieves the necessary parameters for getting all cash transactions."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        // [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<SaveCashViewModel>))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var results = await _cashService.GetAllViewModel();
+            if (results.Any())
+            {
+                return Ok(results);
+            }
+            
+            return NotFound();
+        }
+
+        [HttpPut("update")]
+        [SwaggerOperation(
+            Summary = "Updates a cash transaction.",
+            Description = "Recieves the necessary parameters for updating a cash transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveCashViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(CashViewModel dto, string id)
+        {
+            var request = _mapper.Map<SaveCashViewModel>(dto);
+
+            try
+            {
+                await _cashService.Update(request, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("delete")]
+        [SwaggerOperation(
+            Summary = "Deletes a cash transaction.",
+            Description = "Recieves the necessary parameters for deleting a cash transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                await _cashService.Delete(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+    }
+}
---
+++ WealthMind/Controllers/TransactionController.cs
@@ -0,0 +1,126 @@
+﻿using System.Net.Mime;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using WealthMind.Core.Application.Interfaces.Services;
+using WealthMind.Core.Application.ViewModels.TransactionV;
+
+namespace WealthMind.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [SwaggerTag("Transaction")]
+    public class TransactionController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(IMapper mapper, ITransactionService transactionService)
+        {
+            _mapper = mapper;
+            _transactionService = transactionService;
+        }
+
+        [HttpPost("add")]
+        [SwaggerOperation(
+            Summary = "Registers a transaction.",
+            Description = "Recieves the necessary parameters for registering a transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(SaveTransactionViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Add(SaveTransactionViewModel dto)
+        {
+
+            var results = await _transactionService.Add(dto);
+            if (results.HasError)
+            {
+                return BadRequest(results);
+            }
+            return Ok(results);
+
+        }
+
+        [HttpGet("get")]
+        [SwaggerOperation(
+            Summary = "Gets a transaction.",
+            Description = "Recieves the necessary parameters for getting a transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveTransactionViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Get(string id)
+        {
+            var results = await _transactionService.GetByIdSaveViewModel(id);
+            if (results.HasError)
+            {
+                return BadRequest(results);
+            }
+            return Ok(results);
+        }
+
+        [HttpGet("getall")]
+        [SwaggerOperation(
+            Summary = "Gets all transactions.",
+            Description = "Recieves the necessary parameters for getting all transactions."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetAll()
+        {
+            var results = await _transactionService.GetAllViewModel();
+            if (results.Any())
+            {
+                return Ok(results);
+            }
+
+            return NotFound();
+        }
+
+        [HttpPut("update")]
+        [SwaggerOperation(
+            Summary = "Updates a transaction.",
+            Description = "Recieves the necessary parameters for updating a transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(SaveTransactionViewModel))]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Update(TransactionViewModel dto, string id)
+        {
+            var request = _mapper.Map<SaveTransactionViewModel>(dto);
+
+            try
+            {
+                await _transactionService.Update(request, id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+
+        [HttpDelete("delete")]
+        [SwaggerOperation(
+            Summary = "Deletes a transaction transaction.",
+            Description = "Recieves the necessary parameters for deleting a transaction."
+        )]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> Delete(string id)
+        {
+            try
+            {
+                await _transactionService.Delete(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+            }
+        }
+    }
+}
