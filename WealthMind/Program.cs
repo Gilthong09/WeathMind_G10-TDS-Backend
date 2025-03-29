1996d6ea6fbe026777a1cf92ef44e72d8439a304
Gilthong09 <153387378+Gilthong09@users.noreply.github.com>
2025-03-29 01:28:45 -0400

    Update Program.cs

--- WealthMind/Program.cs
+++ WealthMind/Program.cs
@@ -43,16 +43,8 @@ await app.Services.AddIdentitySeeds();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger(c =>
-    {
-        c.RouteTemplate = "wealth-mind/swagger/{documentName}/swagger.json";  
-    });
-    
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/wealth-mind/swagger/v1/swagger.json", "My API V1");
-        c.RoutePrefix = "wealth-mind/swagger";
-    });
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
