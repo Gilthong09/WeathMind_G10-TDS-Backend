86e3a4f07b7bf2a1af26eae6025390d71011f8e9
Gilthong Palin <gilthongenmanuelpalingarcia@gmail.com>
2025-03-30 20:15:44 +0200

    Some Changes

--- WealthMind.Core.Domain/Entities/Transaction.cs
+++ WealthMind.Core.Domain/Entities/Transaction.cs
@@ -12,7 +12,6 @@ namespace WealthMind.Core.Domain.Entities
         public DateTime TransactionDate { get; set; } = DateTime.UtcNow;
         public string? Description { get; set; }
         public string? CategoryId { get; set; }
-
         public Product? FromProduct { get; set; }
         public Product? ToProduct { get; set; }
         public Category? Category { get; set; }
--- WealthMind.Infrastructure.Identity/Seeds/OthersDefaultUsers.cs
+++ WealthMind.Infrastructure.Identity/Seeds/OthersDefaultUsers.cs
@@ -34,3 +34,5 @@ namespace WealthMind.Infrastructure.Identity.Seeds
         }
     }
 }
+
+
